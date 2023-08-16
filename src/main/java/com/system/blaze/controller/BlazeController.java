ab89548ebc16c50de1fbb1ee5ae5246b6816bc4e
Fred <xuetian.wu@hcl.com>
2023-08-16 11:46:50 -0700

    add lombok and parsing models

--- src/main/java/com/system/blaze/controller/BlazeController.java
+++ src/main/java/com/system/blaze/controller/BlazeController.java
@@ -1,10 +1,13 @@
 package com.system.blaze.controller;
 
+import com.system.blaze.parsingModel.WholeModel;
 import com.system.blaze.service.impl.BlazeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +18,12 @@ public class BlazeController {
 
     @GetMapping(path = "/transactionRiskDecision")
     public ResponseEntity<String> getRisk() {
+        return new ResponseEntity<>("Success", HttpStatus.OK);
+    }
+
+    @PostMapping( "/sendMoneyValidation")
+    public ResponseEntity<?> getRisk2(@RequestBody WholeModel wholeModel) {
+        System.out.println(wholeModel);
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 }
