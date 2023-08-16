c004c6c8c053c27371d7da8ca02d734575f08850
nickpun <nickbpun@gmail.com>
2023-08-15 17:58:20 -0700

    initial commit

---
+++ src/main/java/com/system/blaze/BlazeApplication.java
@@ -0,0 +1,13 @@
+package com.system.blaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlazeApplication.class, args);
+	}
+
+}
