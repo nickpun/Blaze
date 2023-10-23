528c0c3d3147187c62b048a48e29ce38665ab138
nickpun <nickbpun@gmail.com>
2023-10-23 10:21:20 -0700

    renamed endpoint

--- src/main/java/com/system/blaze/controller/BlazeController.java
+++ src/main/java/com/system/blaze/controller/BlazeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/blaze")
+@RequestMapping("/risk")
 public class BlazeController {
 
     private BlazeServiceImpl blazeService;
@@ -21,7 +21,7 @@ public class BlazeController {
         this.blazeService = blazeService;
     }
 
-    @PostMapping(path = "/risk")
+    @PostMapping
     public ResponseEntity<?> getRisk(@RequestBody RiskRequest riskRequest) {
         return blazeService.checkRisk(riskRequest);
     }
